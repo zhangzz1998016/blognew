41f5253da6161857cdb65bbdcb4a8a01c8e08b00
zhangzz1998016 <61784992+zhangzz1998016@users.noreply.github.com>
2021-02-02 13:23:40 +0800

    fabric
    fabric

--- blogproject/settings/common.py
+++ blogproject/settings/common.py
@@ -13,19 +13,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-zm5*w4vex4@y*!_@-2_c*u&@&ueqiyo2)4jym8ho$3_1)gdfw'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.abrightroedeer.icu']
 
 
 # Application definition
